d1e5fec927e7d42344a1c4b8fc06ed95ac7cdbd6
Ameer <ameer.atteya@rapid-develop.com>
2016-11-03 01:49:00 +0200

    notifications for new message

--- Test/web/AppBundle/Resources/public/js/chatting.js
+++ Test/web/AppBundle/Resources/public/js/chatting.js
@@ -1,3 +1,4 @@
+// When a user click on a friend, the chat history appears
 $('.friend').on('click', function () {
     var that = $(this);
 
@@ -16,7 +17,7 @@ $('.friend').on('click', function () {
                 chat_history.append(message_div);
             });
 
-            $('.chat-history').html(chat_history);
+            $('.chat-history').html(chat_history).show();
             $('.chat-history').find('div').scrollTop($('.chat-history').find('div')[0].scrollHeight);
 
             $('.message-textarea, .message-send').show();
@@ -25,6 +26,7 @@ $('.friend').on('click', function () {
     });
 })
 
+// When a user sends a message to a friend
 $('.message-send').on('click', function () {
     var data = [];
     data.push({name: 'message', value: $('.message-textarea').val()});
@@ -47,6 +49,7 @@ $('.message-send').on('click', function () {
     });
 })
 
+// Put the date in the format that is readable for the users
 function getDateFromTimestamp(message_time) {
     var message_time_formatted = message_time.getFullYear() + '-' + message_time.getMonth() + '-' + message_time.getDay() + ' ';
     message_time_formatted += message_time.getHours() < 12 ? message_time.getHours() : (message_time.getHours() - 12);
@@ -54,3 +57,26 @@ function getDateFromTimestamp(message_time) {
     message_time_formatted += message_time.getHours() < 12 ? ' am' : ' pm';
     return message_time_formatted;
 }
+
+// Check for notifications every 5 secs
+(function checkNotifications() {
+    var receiver_id = $('.chat-history:visible').length > 0 ? $('.receiver').val() : -1;
+    $.ajax({
+        url: 'api/v1/checkNotifications/' + receiver_id,
+        success: function (data) {
+            if (data == 1) {
+                $('.notification').html("<div class='alert-info alert-block'>New message</div>");
+            }
+            else {
+                $('.notification').html("");
+            }
+
+        },
+        complete: function () {
+            // Schedule the next request when the current one's complete
+            setTimeout(function () {
+                checkNotifications()
+            }, 5000);
+        }
+    });
+})();
